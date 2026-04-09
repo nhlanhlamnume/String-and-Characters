0b6693ea48f84715a5939664bfb77886b77d8096
rugbratsss <rugbratsss@gmail.com>
2026-04-09 22:34:07 +0200

    Add capitalSentence, capitalized, and capitalized2 classes for sentence capitalization functionality

---
+++ capitalSentence.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class capitalSentence {
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = kb.nextLine();
+
+        String[] words = sentence.split(" ");
+        String results = "";
+
+        for(int i = 0; i < words.length; i++){
+            results += Character.toUpperCase(words[i].charAt(0)) + words[i].substring(1).toLowerCase();
+            if(i < words.length - 1){
+                results += " ";
+            }
+        }
+        System.out.print("Capitalized: " + results);
+    }
+}
